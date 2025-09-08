6410353a9b071bffe9b2ea450f011f32cf4cb1f2
Bhargav Omkar <swapnaomkar080@gmail.com>
2025-09-08 19:07:24 +0530

    Create arraysum.c C file

---
+++ arraysum.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int n, sum=0;
+    printf("Enter array size: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for(int i=0;i<n;i++) { scanf("%d", &arr[i]); sum+=arr[i]; }
+    printf("Sum of array elements = %d\n", sum);
+    return 0;
+}
